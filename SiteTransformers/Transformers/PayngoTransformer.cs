1d7ab298cd407e09015a7b32f36558a4330e65ea
Aviya Duek <aviyad@landa-corp.com>
2025-08-13 17:20:22 +0300

    implement Payngo scraper

--- SiteTransformers/Transformers/PayngoTransformer.cs
+++ SiteTransformers/Transformers/PayngoTransformer.cs
@@ -7,22 +7,35 @@ public class PayngoTransformer : ISiteTransformer
     public List<ProductScrapingRecord> Transform(string htmlContent, string site)
     {
         var doc = new HtmlDocument();
-
         doc.LoadHtml(htmlContent);
-        var productNode = doc.DocumentNode.SelectSingleNode("//div[contains(@class, 'item-root')]");
 
-        // Example: parse message and fill ProductScrapingRecord for SiteB
-        return new List<ProductScrapingRecord>
+        var products = doc.DocumentNode.SelectNodes(".//li[contains(@class, 'item product product-item')]");
+
+        var productScrapingRecords = new List<ProductScrapingRecord>();
+        foreach (var product in products)
         {
-            new(
-                Category: "ExampleCategoryB",
-                Price: "200",
-                SerialNumber: "B456",
-                SiteName: site,
-                Description: htmlContent.ToLower(),
-                SubCategory: "ExampleSubB",
-                DateTime: DateTime.Now
-            )
-        };
+            var description = product.SelectSingleNode(".//strong[contains(@class,'product name product-item-name product_name')]");
+
+            var priceHtml = product.SelectSingleNode(".//span[contains(@id, 'product-price')]");
+            var price = priceHtml.SelectSingleNode(".//span[contains(@class, 'price')]");
+            var priceText = price?.InnerText
+                .Replace("&nbsp;", "")        // Remove HTML entity if present
+                .Replace("\u00A0", "")        // Remove Unicode non-breaking space
+                .Trim();
+
+            var a = price.InnerText;
+            productScrapingRecords.Add(new ProductScrapingRecord(
+                    Category: "Scooters-Bicycles",
+                    Price: priceText,
+                    SerialNumber: "B456",
+                    SiteName: site,
+                    Description: description.InnerText,
+                    SubCategory: "Electric-Scooter",
+                    DateTime: DateTime.Now
+                
+            ));
+        }
+
+        return productScrapingRecords;
     }
 }
